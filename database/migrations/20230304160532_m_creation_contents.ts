ae9b62d6cece67f412c2a65e3fe1b9d85b3029c8
isa.goutama <isagoutama.dev@gmail.com>
2023-03-28 13:44:33 +0700

    embed code field m_creation contents

--- database/migrations/20230304160532_m_creation_contents.ts
+++ database/migrations/20230304160532_m_creation_contents.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("m_creation_contens", function (table) {
         table.uuid("id").primary().notNullable();
         table.string("filename").nullable();
-        table.string("embed_code").nullable();
+        table.text("embed_code").nullable();
         table.uuid("created_by").nullable();
         table.uuid("updated_by").nullable();
         table.timestamps();
