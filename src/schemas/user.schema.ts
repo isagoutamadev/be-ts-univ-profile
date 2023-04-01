2700efffd72ff9ec34994fce718e6af0f64a37f8
isa.goutama <isagoutama.dev@gmail.com>
2023-04-02 00:07:54 +0700

    resolve update user

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,15 +1,5 @@
 import Joi from "joi";
 
-export const UserUpdateSchema = Joi.object({
-    user: Joi.object({
-        role_id: Joi.string().disallow('0001').required(),
-    }).required(),
-    employee: Joi.object({
-        branch_company_id: Joi.string().min(36).max(36).required(),
-        picture: Joi.string().allow("", null).max(100),
-    })
-});
-
 export const CreateUserSchema = Joi.object({
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).min(8).max(30).required(),
     username: Joi.string().alphanum().min(8).required(),
@@ -17,6 +7,12 @@ export const CreateUserSchema = Joi.object({
     role: Joi.string().valid('admin', 'student').required(),
     name: Joi.string().min(3).required(),
     nim: Joi.number().min(5).required(),
+});
+export const UpdateUserSchema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).min(8).max(30).required(),
+    username: Joi.string().alphanum().min(8).required(),
+    password: Joi.string().min(6).required(),
+    role: Joi.string().valid('admin', 'student').required()
 });
 
 export const UserSearchSchema = Joi.object({
