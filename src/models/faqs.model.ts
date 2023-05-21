1d517600dacff788113e5c2d1f0474808b978c1e
isa.goutama <isagoutama.dev@gmail.com>
2023-05-21 11:10:44 +0700

    implement faqs

---
+++ src/models/faqs.model.ts
@@ -0,0 +1,8 @@
+export interface Faqs {
+    id?: string,
+    question?: string,
+    answer?: string,
+    created_by?: string,
+    updated_by?: string,
+    deleted_by?: string,
+}
---
+++ src/resources/faqs/faqs.repository.ts
@@ -0,0 +1,89 @@
+import { Faqs } from "@/models/faqs.model";
+import knex from "@/utils/knex/knex"
+import { Pagination, Paging } from "@/utils/responses/pagination.response";
+
+export class FaqsRepository {
+    async get(search: Faqs, page: number, limit: number): Promise<Paging<Faqs>> {
+        try {
+            const select = [
+                "faq.id",
+                "faq.question",
+                "faq.answer",
+            ];
+
+            const query = knex("m_faqs as faq").select(select);
+            
+            query.whereNull("faq.deleted_at");
+
+            const offset = limit * page - limit;
+            const queryCount = knex().count("id as total").from(knex.raw(`(${query.toQuery()}) x`)).first();
+
+            const [datas, count] = await Promise.all([
+                await query.limit(limit).offset(offset),
+                await queryCount
+            ]);
+            const pagination = new Pagination<Faqs>(
+                datas,
+                //@ts-ignore
+                count.total,
+                page,
+                limit
+            );
+
+            return pagination.getPaging();
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async find(search: Faqs): Promise<Faqs|undefined> {
+        try {
+            const select = [
+                "faq.id",
+                "faq.question",
+                "faq.answer",
+            ];
+
+            const query = knex("m_faqs as faq").select(select);
+
+            query.whereNull("faq.deleted_at");
+
+            return await query.first();
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async create(data: Faqs): Promise<void> {
+        try {
+            await knex("m_faqs").insert({
+                ...data,
+                created_at: knex.raw("now()"),
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async update(data: Faqs): Promise<void> {
+        try {
+            await knex("m_faqs").update({
+                ...data,
+                updated_at: knex.raw("now()"),
+            }).where("id", data.id);
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async delete(data: Faqs): Promise<void> {
+        try {
+            await knex("m_faqs").update({
+                ...data,
+                deleted_at: knex.raw("now()"),
+            }).where("id", data.id);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
